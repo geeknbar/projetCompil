efbbb1f1c3c4828ed89189663437ded55f4380a4
geeknbar <dorian.coffinet@gmail.com>
2014-03-26 15:34:12 +0100

    Refactoring des dossiers, lexeur fonctionne, création du fichier de grammaire, makefile du prof ajouter à la place du notre.

---
+++ src/grammaire.tab.h
@@ -0,0 +1,82 @@
+/* A Bison parser, made by GNU Bison 2.5.  */
+
+/* Bison interface for Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989-1990, 2000-2011 Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     ASSIGNMENT = 258,
+     COLON = 259,
+     DIV = 260,
+     DO = 261,
+     ELSE = 262,
+     END = 263,
+     EQUAL = 264,
+     FUNCTION = 265,
+     GE = 266,
+     GT = 267,
+     IDENTIFIER = 268,
+     IF = 269,
+     LE = 270,
+     LPAREN = 271,
+     LT = 272,
+     MINUS = 273,
+     MOD = 274,
+     NOTEQUAL = 275,
+     PBEGIN = 276,
+     PLUS = 277,
+     PROGRAM = 278,
+     RPAREN = 279,
+     SEMICOLON = 280,
+     DIVQUOT = 281,
+     MULTIPLICAT = 282,
+     THEN = 283,
+     VAR = 284,
+     WHILE = 285
+   };
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+
---
+++ src/grammaire.y
@@ -0,0 +1,41 @@
+%{
+	#include<stdio.h>
+	int yylex();
+	void yyerror(char const* s);
+	extern FILE *yyin;
+%}
+
+%token ASSIGNMENT COLON DIV DO
+%token ELSE END EQUAL
+%token FUNCTION GE GT IDENTIFIER IF
+%token LE LPAREN LT MINUS MOD NOTEQUAL
+%token PBEGIN PLUS
+%token PROGRAM RPAREN
+%token SEMICOLON DIVQUOT MULTIPLICAT THEN
+%token VAR WHILE
+
+%error-verbose
+
+%%
+
+program : PROGRAM IDENTIFIER SEMICOLON {};
+
+%%
+
+int main(int argc, char* argv[]){
+	FILE *f = NULL;
+	if(argc >1) {
+		f = fopen(argv[1],"r");
+			if(f==NULL){
+				return -1;
+			}
+			yyin=f;
+	}
+	yyparse();
+	if(f!=NULL)
+		fclose(f);
+}
+
+void yyerror(char const* s){
+	fprintf(stderr, "Erreur %s\n", s);
+}
