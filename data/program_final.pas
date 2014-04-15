920f4ea906454a76a230e869f52908f8e1e12f5a
geeknbar <dorian.coffinet@gmail.com>
2014-04-15 17:49:31 +0200

    Remise à plat du programme final.

--- data/program_final.pas
+++ data/program_final.pas
@@ -2,13 +2,13 @@ PROGRAM prog;
 
 { function f }
 FUNCTION f(x:INTEGER) :INTEGER;
-VAR pp,yy:INTEGER;
+VAR i:INTEGER;
 BEGIN
 	i := 0;
 	f := 0;
 	WHILE i < x DO
 	BEGIN
-		IF i + 2 = 0 THEN
+		IF i MOD 2 = 0 THEN
 			f := f + i;
 	END;
 END;
@@ -16,5 +16,6 @@ END;
 { main program }
 VAR x: INTEGER;
 BEGIN
- o:=0;
+ READLN(x);
+ WRITELN(f(x));
 END.
